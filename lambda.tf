76ac55198b0f0cbcb93668e09f36ac171efaafb8
fooweiguo <fooweiguo@hotmail.com>
2023-11-25 11:58:55 +0800

    test

--- lambda.tf
+++ lambda.tf
@@ -8,11 +8,11 @@ data "archive_file" "lambda" {
 
 # Defines the lambda function to be created using file
 
-resource "aws_lambda_function" "lambda_function" {
-  function_name    = var.lambda_function_name
+resource "aws_lambda_function" "own_lambda" {
+  function_name    = "lambda_function"
   handler          = "lambda_function.lambda_handler"
   runtime          = "nodejs14.x"
   role              = aws_iam_role.iam_for_lambda.arn
-  filename         = lambda_function.py                      # Adjust the path
-  source_code_hash = lambda_function.output_base64sha256               # Adjust the path
+  filename         = "${var.lambda_file_name}.zip"                      # Adjust the path
+  source_code_hash = data.archive_file.lambda.output_base64sha256               # Adjust the path
 }
