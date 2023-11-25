dda0a8909c6f22cf39ff2dfb4e64d5ddd86723b1
fooweiguo <68820158+fooweiguo@users.noreply.github.com>
2023-11-25 10:42:29 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 
 # Configure the AWS Provider
 # Uncomment the region, access_key and secret_key if you are running locally
-provider "aws" {
+#provider "aws" {
   #region     = "us-east-1"                # Update accordingly
   #access_key = "xxx"                     # Update accordingly
   #secret_key = "xxx"                     # Update accordingly
-}
+#}
