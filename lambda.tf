bc47a10ca144e492561a139c3ba4f96cf2fdc40d
fooweiguo <fooweiguo@hotmail.com>
2023-11-25 11:21:50 +0800

    test

--- lambda.tf
+++ lambda.tf
@@ -9,7 +9,7 @@ data "archive_file" "lambda" {
 # Defines the lambda function to be created using file
 
 resource "aws_lambda_function" "own_lambda" {
-  function_name    = var.lambda_function_name.default
+  function_name    = var.lambda_function_name
   handler          = "lambda_function.lambda_handler"
   runtime          = "nodejs14.x"
   role              = aws_iam_role.iam_for_lambda.arn
