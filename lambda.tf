0b93a8929e6ab388bbceb81d9caa8f443adc29e9
fooweiguo <fooweiguo@hotmail.com>
2023-11-25 12:01:00 +0800

    test

--- lambda.tf
+++ lambda.tf
@@ -13,6 +13,6 @@ resource "aws_lambda_function" "own_lambda" {
   handler          = "lambda_function.lambda_handler"
   runtime          = "nodejs14.x"
   role              = aws_iam_role.iam_for_lambda.arn
-  filename         = "${var.lambda_file_name}.zip"                      # Adjust the path
+  filename         = "files.zip"                      # Adjust the path
   source_code_hash = data.archive_file.lambda.output_base64sha256               # Adjust the path
 }
